54355aad6bf24ac7a843cc2b6db34e327d319ff2
Zaid Alyafeai <alyafey22@gmail.com>
2018-10-18 21:29:53 +0300

    Update data.py

--- data.py
+++ data.py
@@ -1,1 +1,51 @@
+class_names = ['cloud', 'sun', 'pants', 'umbrella', 'table', 'ladder', 'eyeglasses', 'clock', 'scissors', 'cup']
 
+import urllib.request
+import os 
+
+def download_and_load(max_items_per_class = 10000):
+  root = 'data'
+  os.mkdir('data')
+  print('downloading ...')
+  base = 'https://storage.googleapis.com/quickdraw_dataset/full/numpy_bitmap/'
+  for c in class_names:
+    path = base+c+'.npy'
+    print(path)
+    urllib.request.urlretrieve(path, f'{root}/{c}.npy')
+  print('loading ...')
+  
+  #initialize variables 
+  x = np.empty([0, 784])
+  y = np.empty([0])
+
+  #load each data file 
+  for idx, file in enumerate(class_names):
+      data = np.load(f'{root}/{file}.npy')
+      data = data[0: max_items_per_class, :]
+      labels = np.full(data.shape[0], idx)
+
+      x = np.concatenate((x, data), axis=0)
+      y = np.append(y, labels)
+
+  data = None
+  labels = None
+
+  #randomize the dataset 
+  permutation = np.random.permutation(y.shape[0])
+  x = x[permutation, :]
+  y = y[permutation]
+
+  #reshape and inverse the colors 
+  x = 255 - np.reshape(x, (x.shape[0], 28, 28))
+
+  #separate into training and testing 
+  vfold_size = int(x.shape[0]/100*(vfold_ratio*100))
+
+  x_test = x[0:vfold_size, :]
+  y_test = y[0:vfold_size]
+
+  x_train = x[vfold_size:x.shape[0], :]
+  y_train = y[vfold_size:y.shape[0]]
+  print('Training Data : ', x_train.shape[0])
+  print('Testing  Data : ', x_test.shape[0])
+  return x_train, y_train, x_test, y_test
